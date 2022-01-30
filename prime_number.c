34e15112387753da99dfd40d7910b4de411a8610
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-01-30 21:06:26 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d", &a);     if(a%2 == 0 || a%3 == 0 || a%4 == 0 || a%5 == 0 || a%6 == 0 || a%7 == 0 || a%8 == 0 || a%9 == 0)     {         printf("not a prime");     }     else     {         printf("prime");     }     return 0; }

---
+++ prime_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if(a%2 == 0 || a%3 == 0 || a%4 == 0 || a%5 == 0 || a%6 == 0 || a%7 == 0 || a%8 == 0 || a%9 == 0)
+    {
+        printf("not a prime");
+    }
+    else
+    {
+        printf("prime");
+    }
+    return 0;
+}
