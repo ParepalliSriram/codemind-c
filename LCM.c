3777ab31857c5af056e19894c456cd1144fe9b7f
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-02-08 22:21:00 +0530

    #include<stdio.h> int main() {     int a, b, c, d;     scanf("%d %d", &a, &b);     for(c=1; c<=b; c++)     {         d=a*c;         if(d%b == 0)         {             printf("%d", d);             break;         }     }     return 0; }

---
+++ LCM.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d", &a, &b);
+    for(c=1; c<=b; c++)
+    {
+        d=a*c;
+        if(d%b == 0)
+        {
+            printf("%d", d);
+            break;
+        }
+    }
+    return 0;
+}
