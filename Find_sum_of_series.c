8b89e731d18dbd604f64d06ea1f9e58c500132c9
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-02-16 22:03:21 +0530

    #include<stdio.h> int main() {     float a, b, c=0, i;     scanf("%f", &a);     for(i=1; i<=a; i++)     {         b=1/i;         c+=b;     }     printf("%.2f", c);     return 0; }

---
+++ Find_sum_of_series.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float a, b, c=0, i;
+    scanf("%f", &a);
+    for(i=1; i<=a; i++)
+    {
+        b=1/i;
+        c+=b;
+    }
+    printf("%.2f", c);
+    return 0;
+}
