3b8e255faa76c50ca89fd4fa2faede33ea095de3
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-05-16 20:09:19 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",64+i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -7,14 +7,7 @@ int main()
     {
         for(j=1;j<=n;j++)
         {
-            if (i==j)
-            {
-                printf("0");
-            }
-            else
-            {
-                printf("x");
-            }
+            printf("%c ",64+i);
         }
         printf("
 ");
