f0f6573231d9df9dab4d498f3ff76faae19885c5
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-06-19 11:30:29 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[100],c,i,j,k,l,max=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&c);     for(i=0;i<n;i++)     {         if(a[i]>max)         {             max=a[i];         }     }     for(i=0;i<n;i++)     {         if(max-a[i]<=c)         {             printf("True ");         }         else         {             printf("False ");         }     } }

---
+++ Greatest_Number_of_Candies.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[100],c,i,j,k,l,max=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&c);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(max-a[i]<=c)
+        {
+            printf("True ");
+        }
+        else
+        {
+            printf("False ");
+        }
+    }
+}
