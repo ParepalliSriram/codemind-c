d6f6b577b132f17b9a7434a59bd7d100cbac3cb2
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-09-23 14:32:48 +0530

    #include<stdio.h> int main() {     int r,c;     scanf("%d %d",&r,&c);     int i,j,k=0,l,arr[100][100],co=0,ar2[100];     for(i=0;i<r;i++)     {         for(l=0;l<c;l++)         {             scanf("%d",&arr[i][l]);         }     }
        for(i=0;i<c;i++)
        {
            for(l=0;l<r;l++)
            {
            	co+=arr[l][i];
            }
            ar2[k++]=co;
            co=0;
        }
        
        int max=ar2[0];
        for(i=0;i<k;i++)
        {
            if(ar2[i]>=max)
            {
                max=ar2[i];
            }
        }
        printf("%d",max);
    }

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int i,j,k=0,l,arr[100][100],co=0,ar2[100];
+    for(i=0;i<r;i++)
+    {
+        for(l=0;l<c;l++)
+        {
+            scanf("%d",&arr[i][l]);
+        }
+    }
+    
+    for(i=0;i<c;i++)
+    {
+        for(l=0;l<r;l++)
+        {
+        	co+=arr[l][i];
+        }
+        ar2[k++]=co;
+        co=0;
+    }
+    
+    int max=ar2[0];
+    for(i=0;i<k;i++)
+    {
+        if(ar2[i]>=max)
+        {
+            max=ar2[i];
+        }
+    }
+    printf("%d",max);
+}
