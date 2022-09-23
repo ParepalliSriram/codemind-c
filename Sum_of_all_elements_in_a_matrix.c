45ff236c547250289c09aa9b58653048c16ada65
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-09-23 15:06:36 +0530

    #include<stdio.h> int main() {     int r,c;     scanf("%d %d",&r,&c);     int i,j,k=0,l,arr[100][100],co=0;     for(i=0;i<r;i++)     {         for(l=0;l<c;l++)         {             scanf("%d",&arr[i][l]);             co+=arr[i][l];         }     }     printf("%d",co); }

---
+++ Sum_of_all_elements_in_a_matrix.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int i,j,k=0,l,arr[100][100],co=0;
+    for(i=0;i<r;i++)
+    {
+        for(l=0;l<c;l++)
+        {
+            scanf("%d",&arr[i][l]);
+            co+=arr[i][l];
+        }
+    }
+    printf("%d",co);
+}
