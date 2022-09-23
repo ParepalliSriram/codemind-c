708036f9b61b21229648082829e401018d8a0eda
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-09-23 14:25:21 +0530

    #include<stdio.h> int main() {     int r,c;     scanf("%d %d",&r,&c);     int i,j,k=0,l,arr[100][100],co=0;     for(i=0;i<r;i++)     {         for(l=0;l<c;l++)         {             scanf("%d",&arr[i][l]);         }     }     for(i=0;i<r;i++)     {         for(l=0;l<c;l++)         {         	if((l==0 || l==c-1)||(i==0 || i==r-1))         	{         		co+=arr[i][l]; 			}         }     }     printf("%d",co); }

---
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int i,j,k=0,l,arr[100][100],co=0;
+    for(i=0;i<r;i++)
+    {
+        for(l=0;l<c;l++)
+        {
+            scanf("%d",&arr[i][l]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(l=0;l<c;l++)
+        {
+        	if((l==0 || l==c-1)||(i==0 || i==r-1))
+        	{
+        		co+=arr[i][l];
+			}
+        }
+    }
+    printf("%d",co);
+}
