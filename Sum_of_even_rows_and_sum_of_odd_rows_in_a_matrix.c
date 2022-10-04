1f7d5e1eeebe11428020462ded67f052f4523d95
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-10-04 10:49:21 +0530

    #include<stdio.h> int main() {     int r,c;     scanf("%d%d",&r,&c);     int i,j,k,arr[r][c];     for(i=0;i<r;i++)     {         for(k=0;k<c;k++)         {             scanf("%d",&arr[i][k]);         }     }     int ev=0,od=0;     for(i=0;i<r;i++)     {         for(k=0;k<c;k++)         {             if(i%2==0)             {                 ev+=arr[i][k];             }             else             {                 od+=arr[i][k];             }         }     }     printf("%d %d",ev,od); }

--- Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
@@ -2,28 +2,29 @@
 int main()
 {
     int r,c;
-    scanf("%d %d",&r,&c);
-    int i,j,k=0,l,arr[100][100],co=0,co2=0,ar2[100];
+    scanf("%d%d",&r,&c);
+    int i,j,k,arr[r][c];
     for(i=0;i<r;i++)
     {
-        for(l=0;l<c;l++)
+        for(k=0;k<c;k++)
         {
-            scanf("%d",&arr[i][l]);
+            scanf("%d",&arr[i][k]);
         }
     }
+    int ev=0,od=0;
     for(i=0;i<r;i++)
     {
-        for(l=0;l<c;l++)
+        for(k=0;k<c;k++)
         {
             if(i%2==0)
             {
-        	    co+=arr[i][l];
+                ev+=arr[i][k];
             }
             else
             {
-                co2+=arr[i][l];
+                od+=arr[i][k];
             }
         }
     }
-    printf("%d %d",co,co2);
+    printf("%d %d",ev,od);
 }
