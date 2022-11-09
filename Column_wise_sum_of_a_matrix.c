db4eaccbd2d6bc19c5e595f15fa1423f01694393
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-11-09 07:40:14 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d%d",&n,&m);     int i,j,k,l,r=0,c=0,arr[n][m],arrr[100];     for(i=0;i<n;i++)     {         for(k=0;k<m;k++)         {             scanf("%d",&arr[i][k]);         }     }     for(i=0;i<m;i++)     {         for(k=0;k<n;k++)         {             r+=arr[k][i];         }         printf("%d ",r);         r=0;     }
    }

--- Column_wise_sum_of_a_matrix.c
+++ Column_wise_sum_of_a_matrix.c
@@ -1,23 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int r,c;
-    scanf("%d %d",&r,&c);
-    int i,j,k=0,l,arr[100][100],co=0,ar2[100];
-    for(i=0;i<r;i++)
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int i,j,k,l,r=0,c=0,arr[n][m],arrr[100];
+    for(i=0;i<n;i++)
     {
-        for(l=0;l<c;l++)
+        for(k=0;k<m;k++)
         {
-            scanf("%d",&arr[i][l]);
+            scanf("%d",&arr[i][k]);
         }
     }
-    for(i=0;i<c;i++)
+    for(i=0;i<m;i++)
     {
-        for(l=0;l<r;l++)
+        for(k=0;k<n;k++)
         {
-        	co+=arr[l][i];
+            r+=arr[k][i];
         }
-        printf("%d ",co);
-        co=0;
+        printf("%d ",r);
+        r=0;
     }
+    
 }
